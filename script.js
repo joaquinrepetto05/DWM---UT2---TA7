48aaad28dd6bef6738abec0941ede5b16467a785
Joaquin <joaquin.repetto@correo.ucu.edu.uy>
2024-08-15 21:18:16 -0300

    TA 7 - UT2

---
+++ script.js
@@ -0,0 +1,49 @@
+function getTheTitles(books) {
+    var titles = [];
+
+    for (var book of books) {
+        titles.push(book.title);
+    }
+
+    return titles;
+}
+
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    },
+    {
+        title: 'Book3',
+        author: 'Name3'
+    },
+    {
+        title: 'Book4',
+        author: 'Name4'
+    },
+    {
+        title: 'Book5',
+        author: 'Name5'
+    }
+];
+
+console.log(getTheTitles(books));
+
+// OTRAS ALTERNATIVAS:
+/*
+
+function getTheTitles(books) {
+    return books.map(book => book.title); // Se usa el método .map()
+}
+
+function getTheTitles(books) {
+    var titles = [];
+    books.forEach(book => titles.push(book.title)); // Se usa el método .forEach()
+    return titles;
+}
+
+*/
